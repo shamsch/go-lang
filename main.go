52a6cf469240a693731a3f05b609c141b0c3726b
Shamsur <shamsur314@gmail.com>
2022-11-30 19:07:21 +0200

    takes input

--- main.go
+++ main.go
@@ -13,7 +13,18 @@ func main() {
 	// read more about different ways % is used in formatting string here: https://golang.org/pkg/fmt/
 	fmt.Printf("Welcome to the %T\n", eventName)
 	fmt.Printf("Total guests allowed: %d\n", totalGuests)
-	fmt.Printf("Ticket price: $%.3f\n", ticketPrice)
+	fmt.Printf("Ticket price: $%.2f\n", ticketPrice)
 	fmt.Printf("Tickets left: %d\n", ticketLeft)
+
+	// user name and number of tickets
+	var userName string
+	var tickets uint
+
+	// taking user input
+	// variable value is scanned using the pointer of the variable (&)
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&userName)
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&tickets)
 
 }
